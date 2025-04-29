78fcf9b130fa5fecd35129575b0c5d46187a6b96
YordanovDY <yordanovdy23@gmail.com>
2025-04-29 12:13:08 +0300

    Check data type before saving to database

---
+++ src/enums/task-enums.ts
@@ -0,0 +1,24 @@
+export enum Category {
+    Planning = 'Planning',
+    Implementation = 'Implementation',
+    ProblemSolving = 'Problem Solving',
+    Testing = 'Testing',
+    Deployment = 'Deployment',
+    Documentation = 'Documentation',
+    Learning = 'Learning',
+    Meeting = 'Meeting',
+    Other = 'Other'
+}
+
+export enum Status {
+    Pending = 'Pending',
+    InProgress = 'In Progress',
+    Completed = 'Completed'
+}
+
+export enum Priority {
+    Low = 'Low',
+    Medium = 'Medium',
+    High = 'High',
+    Critical = 'Critical'
+}
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,12 +1,13 @@
 import { Schema, model } from "mongoose";
 import { ITask } from "../types/task-type";
+import { Category, Priority, Status } from "../enums/task-enums";
 
 const taskSchema = new Schema<ITask>({
-    date: { type: Date },
-    category: { type: String },
-    status: { type: String },
-    priority: { type: String },
-    description: { type: String },
+    date: { type: Date, required: true },
+    category: { type: String, enum: Object.values(Category), required: true },
+    status: { type: String, enum: Object.values(Status), required: true },
+    priority: { type: String, enum: Object.values(Priority), required: true },
+    description: { type: String, required: true },
 })
 
 const Task = model<ITask>('Task', taskSchema);
