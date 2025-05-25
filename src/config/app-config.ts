271e2cc9212c2f080df9ca27990015a4f74249bc
YordanovDY <yordanovdy23@gmail.com>
2025-05-25 11:46:55 +0300

    Change the port

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -3,11 +3,14 @@ import cors from 'cors';
 import { attachErrorResponses } from '../middlewares/error-middleware';
 
 export default function expressInit(): Application {
-    const PORT = 3000;
+    const PORT = 3003;
     const app = express();
 
     app.use(express.json());
-    app.use(cors());
+    app.use(cors({
+        origin: 'http://localhost:5001',
+        credentials: true,
+    }));
     app.use(attachErrorResponses);
 
     app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
